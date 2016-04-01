a28734a85de742ebaefacae2c318346a7e21c29f
erikmunkby <erik.munkby@gmail.com>
2016-04-01 12:36:46 +0200

    it's done.

--- src/Lab1/PDFDownloader.java
+++ src/Lab1/PDFDownloader.java
@@ -1,7 +1,12 @@
 package Lab1;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,7 +16,7 @@ import java.util.regex.Pattern;
  */
 public class PDFDownloader {
     public static void main(String[] args){
-        download("http://cs.lth.se/eda095/foerelaesningar/?no_cache=1");
+        download("http://cs.lth.se/eda095/tentamen/");
     }
 
     private static void download(String url){
@@ -28,13 +33,31 @@ public class PDFDownloader {
 
         Pattern htmltag = Pattern.compile("<a\\b[^>]*href=\"[^>]*>(.*?)</a>");
         Pattern link = Pattern.compile("href=\\\"[^>]*\\\">");
+        Pattern pdf = Pattern.compile("(.*)\\.pdf");
         Matcher tagMatch = htmltag.matcher(content);
 
+        int i = 1;
         while(tagMatch.find()){
             Matcher m = link.matcher(tagMatch.group());
             while (m.find()){
-                System.out.println(m.group(0));
+                Matcher pdfM = pdf.matcher(m.group());
+
+                if (pdfM.find()){
+                    String pdfUrl = pdfM.group(0).replaceAll("href=\"", "");
+                    InputStream in;
+                    try {
+                        URL dl = new URL(pdfUrl);
+                        in = dl.openStream();
+                        System.out.println(pdfUrl);
+                        //Files.copy(in, Paths.get("download" + i + ".pdf"), StandardCopyOption.REPLACE_EXISTING);
+                        i++;
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
+        System.out.println("Found " + i + " pdf documents.");
     }
 }
